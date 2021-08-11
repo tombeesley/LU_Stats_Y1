00a9a7b2968bac1d1e47e742f83c6e9f28b7dc35
tombeesley <t.beesley@lancaster.ac.uk>
2021-08-11 12:29:55 +0100

    update associated files

--- files OLD/Week_7/Week_7_Task_2.R
+++
@@ -1,7 +0,0 @@
-# Week_7_Task_2
-library(tidyverse)
-data_phone <- read_csv("data_phone.csv")
-
-data_phone %>%
-  ggplot() +
-  geom_point() # you will need to edit this code
--- files/randomise_data.R
+++ files/randomise_data.R
@@ -15,25 +15,46 @@ penelope %>%
          oweight = case_when(identity == "Other" ~ estimate)) %>% 
   write_csv("Week_2/penelope.csv")
 
-# Week 6 create random data
+
+# Week 6 
 
 data <- read_csv("Week_6/location_music_Wk6.csv")
 
-# remove conversation skills
-data %>% 
-  select(-conv_skills) %>% 
-  write_csv("Week_6/location_music_Wk6.csv")
+data <- data[sample(nrow(data)),] # shuffle rows
 
-swift <- sample(1:5,204, replace = TRUE)
-beyonce <- sample(1:5,204, replace = TRUE)
-sheeran <- sample(1:5,204, replace = TRUE)
+write_csv(data, "Week_6/location_music_Wk6.csv")
 
-mean(data$music_swift, na.rm = TRUE)
-mean(beyonce)
-mean(sheeran)
 
-# Week 7 create
+# Week 7 
 
 data <- read_csv("Week_7/data_salary_Wk7.csv")
+
+# add random element to salary estimates
+
+re <- sample(-3:3,205, replace = TRUE) # add a random element
+
+data$uk_salary <- data$uk_salary + re*1000
+
+write_csv(data, "Week_7/data_salary_Wk7.csv")
+
+
+# Week 8
   
+data <- read_csv("Week_8/data_stroop_Wk8.csv")
+
+re <- sample(seq(-.5,.5,.1),606, replace = TRUE) # add a random element
+
+data$stroop_time <- data$stroop_time + re
+
+write_csv(data, "Week_8/data_stroop_Wk8.csv")
+
+
+# Week 9
+
 data <- read_csv("Week_9/gamble_salary_Wk9.csv")
+
+re <- sample(-3:3,204, replace = TRUE) # add a random element
+
+data$salary_estimate <- data$salary_estimate + re*1000
+
+write_csv(data, "Week_9/gamble_salary_Wk9.csv")
